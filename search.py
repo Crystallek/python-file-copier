0ae02e2c50df4d150631438d353b334bf28694b4
Crystallek <99501451+Crystallek@users.noreply.github.com>
2022-08-04 16:11:44 +0200

    substrings update

--- search.py
+++ search.py
@@ -5,7 +5,7 @@ import random
 
 startingPath = input("Starting directory: ")
 endingPath = input("Ending directory: ")
-fileExtension = input("File extension to search for (if you want to add more extension, divide them by ',', something like '.png,.jpg' ): ").split(",")
+fileExtension = input("Substring to search for (if you want to add more substrings, divide them by ',', something like '.png,.jpg') (not case sensitive): ").split(",")
 foundedFiles = []
 fileCounter = 0
 copiedFilesCounter = 0
@@ -15,7 +15,7 @@ if os.path.isdir(startingPath):
         for path, subdirs, files in os.walk(startingPath):
             for name in files:
                 for fileExt in fileExtension:
-                    if name.endswith(fileExt):
+                    if fileExt.lower() in name.lower():
                         fileCounter += 1
                         foundedFiles.append(name)
                         fileCount = len(list(set(foundedFiles)))
@@ -23,17 +23,17 @@ if os.path.isdir(startingPath):
 
         fileCounter = int(len(list(set(foundedFiles))))
         if len(fileExtension) == 1:
-            print(f"{fileCounter} files found with an extension {fileExtension}.")
+            print(f"{fileCounter} files found with a substring {fileExtension}.")
         if len(fileExtension) >= 2:
-            print(f"{fileCounter} files found with extensions {fileExtension}.")
-            
+            print(f"{fileCounter} files found with substrings {fileExtension}.")
+
         print("Copying will start in 3 seconds.")
         time.sleep(3)
 
         for path, subdirs, files in os.walk(startingPath):
             for name in files:
                 for fileExt in fileExtension:
-                    if name.endswith(fileExt) and not name in os.listdir(endingPath):
+                    if fileExt.lower() in name.lower() and not name in os.listdir(endingPath):
                         try:
                             shutil.copy(path + "/" + name, endingPath)
                             copiedFilesCounter += 1
@@ -51,7 +51,7 @@ if os.path.isdir(startingPath):
         if copiedFilesCounter == 0 and fileCounter != 0:
             print("No files were copied because they were already in the ending folder.")
         if fileCounter == 0:
-            print(f"No files were copied because no file has the extension {fileExtension}.")
+            print(f"No files were copied because no file's name has the substring {fileExtension} in it.")
         if copiedFilesCounter != fileCounter:
             print(f"Successfully copied {copiedFilesCounter} files!\nNot all files were copied because some were in the directory already.")
         else:
